361f16d1b5b7468e96af9c1f2f11ca9ae5038d78
Tanner253 <PercivalTanner@gmail.com>
2022-04-02 14:02:12 -0700

    adds

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -3,7 +3,7 @@
 const userModel = require('./users.js');
 const { Sequelize, DataTypes } = require('sequelize');
 
-const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:auth:';
+const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:auth:' || 'postgresql://localhost:5432/localDb';
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialectOptions: {
