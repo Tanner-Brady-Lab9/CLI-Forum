15a77192cbf29338b8caf59836dbd3a24908d017
Tanner253 <PercivalTanner@gmail.com>
2022-04-02 13:45:20 -0700

    adds working tests

--- src/auth/middleware/acl.js
+++ src/auth/middleware/acl.js
@@ -1,20 +1,15 @@
 'use strict';
 
 module.exports = (capability) => {
-
   return (req, res, next) => {
     try {
-      console.log("ACL LOG", req.user.capabilities);
-      // [ 'read' ]                        'user' 
       if (req.user.capabilities.includes(capability)) {
         next();
-      }
-      else {
+      } else {
         next('Access Denied');
       }
     } catch (e) {
       next('Invalid Login');
     }
   };
-
 };
