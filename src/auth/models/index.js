f273c1b3feab132d7eb284fd45018f2293715560
Tanner253 <PercivalTanner@gmail.com>
2022-04-02 14:48:55 -0700

    Adds?

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -3,12 +3,12 @@
 const userModel = require('./users.js');
 const { Sequelize, DataTypes } = require('sequelize');
 
-const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:auth:' || 'postgresql://localhost:5432/localDb';
+const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:auth:';
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialectOptions: {
     ssl: {
-      require: true,
+      require: false,
       rejectUnauthorized: false,
     },
   },
