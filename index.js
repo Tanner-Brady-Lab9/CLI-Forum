04f4335a1a769859d88e9e5fcf2cb50fd259310e
Tanner253 <PercivalTanner@gmail.com>
2022-04-04 08:11:20 -0700

    Adds

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ const server = require('./src/server.js');
 
 
 db.sync().then(async () => {
-  await posts.create({
-    userId: 1,
-    subject: 'Test',
-    content: 'We are testing',
-  });
+  // await posts.create({
+  //   userId: 1,
+  //   subject: 'Test',
+  //   content: 'We are testing',
+  // });
   authDb.sync().then(async() => {
-    await users.create({
-      username: 'admin',
-      password: 'password',
-      role: 'admin',
-    });
-    await users.create({
-      username: 'writer',
-      password: 'password',
-      role: 'writer',
-    });
-    await users.create({
-      username: 'editor',
-      password: 'password',
-      role: 'editor',
-    });
+    // await users.create({
+    //   username: 'admin',
+    //   password: 'password',
+    //   role: 'admin',
+    // });
+    // await users.create({
+    //   username: 'writer',
+    //   password: 'password',
+    //   role: 'writer',
+    // });
+    // await users.create({
+    //   username: 'editor',
+    //   password: 'password',
+    //   role: 'editor',
+    // });
     server.start(process.env.PORT||3000);
   });
 });
